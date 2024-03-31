624f6a6c482da5787c9b13128a1c2350c911dc5f
kth <rnjsxo1017@gmail.com>
2024-03-31 23:33:36 +0900

    ~참조

--- src/ref/MethodChange1.java
+++ src/ref/MethodChange1.java
@@ -9,6 +9,7 @@ public class MethodChange1 {
         System.out.println("메서드 호출 후: a = " + a);
     }
 
+    // 당연히 x는 메서드 종료 후 제거되므로 위 코드의 메서드는 아무 의미없음
     static void changePrimitive(int x) {
         x = 20;
     }
--- src/ref/MethodChange2.java
+++ src/ref/MethodChange2.java
@@ -11,6 +11,7 @@ public class MethodChange2 {
         System.out.println("메서드 호출 후: dataA.value = " + dataA.value);
     }
 
+    // dataX 인스턴스도 메모리에 생성
     static void changeReference(Data dataX) {
         System.out.println("dataX=" + dataX);
         dataX.value = 20;
