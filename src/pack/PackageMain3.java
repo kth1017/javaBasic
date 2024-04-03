5288dfc0e1b42c5eacf0ef77a6f71a70e137bae1
kth <rnjsxo1017@gmail.com>
2024-04-03 21:58:49 +0900

    ~캡슐화

--- src/pack/PackageMain3.java
+++ src/pack/PackageMain3.java
@@ -4,6 +4,7 @@ import pack.a.User;
 
 public class PackageMain3 {
 
+    // 중복 클래스의 경우 선착순
     public static void main(String[] args) {
         User userA = new User();
         pack.b.User userB = new pack.b.User();
