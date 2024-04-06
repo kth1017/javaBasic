4eb02fd51463d158d2deae63d416421c81901cd0
kth <rnjsxo1017@gmail.com>
2024-04-06 23:11:03 +0900

    상속

--- src/final1/ConstructInit.java
+++ src/final1/ConstructInit.java
@@ -1,7 +1,7 @@
 package final1;
 
 public class ConstructInit {
-
+    // final을 멤버 변수에 사용시 선언시 초기화가 아니면 반드시 생성자로만 할당 가능
     final int value;
 
     public ConstructInit(int value) {
--- src/final1/FieldInit.java
+++ src/final1/FieldInit.java
@@ -2,6 +2,7 @@ package final1;
 
 public class FieldInit {
 
+    // final 필드 초기화시 메모리 중복의 관점에서 static이 일반적
     static final int CONST_VALUE = 10;
     final int value = 10;
 
